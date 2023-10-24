f65aad172c1a5bea96d7e5734ed4eebe80995d56
zKlau <claudiupadure50@yahoo.com>
2023-10-24 22:26:46 +0300

    updated the tooltip system

--- Prefabs/User Interface/Buttons/Tooltip.gd
+++ Prefabs/User Interface/Buttons/Tooltip.gd
@@ -1,19 +1,24 @@
 extends Control
 
 #@export var background_texture : Texture
-@onready var description = $Description
+@onready var description = $PanelContainer/Description
 @onready var background = $Background
+@export var label_teture : Texture2D
 
 func set_text(text : String):
 	description.text = text
+	
+func _process(delta):
+	if background.scale.y != 0.0005 * description.size.y and len(description.text) != 0:
+		background.scale.y = 0.0005 * description.size.y + 0.016
+	
 
 
 func _on_area_2d_mouse_entered():
-	#background.texture = background_texture
+	background.scale.y = 0.0005 * description.size.y + 0.016
+	print(description.size.y)
 	background.visible = true
 	description.visible = true
-	#description.text = get_parent().ability.description
-	print("mouse entered the ebility slot")
 	pass # Replace with function body.
 
 
