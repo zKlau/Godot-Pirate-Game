7617cb5d97eb1a0bc30805075a60d0d8eecb5815
zKlau <claudiupadure50@yahoo.com>
2023-10-28 23:52:13 +0300

    added special disable button

--- Prefabs/User Interface/Ship/Special_ability_button.gd
+++ Prefabs/User Interface/Ship/Special_ability_button.gd
@@ -1,6 +1,7 @@
 extends TextureButton
 
 @export_multiline var description : String
+@onready var parent = $"../../.."
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Tooltip.set_text(description)
@@ -9,4 +10,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if parent.ship.attacked or !parent.ship.can_attack:
+		disable_button()
+	else:
+		enable_button()
+		
+
+func disable_button():
+	$lock.visible = true
+	disabled = true
+func enable_button():
+	$lock.visible = false
+	disabled = false
+
 	pass
