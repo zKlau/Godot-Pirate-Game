06575fb85b3eaeae8f2fa72165414661a9cfb5e5
zKlau <claudiupadure50@yahoo.com>
2023-11-11 03:39:18 +0200

    added infinite ocean

--- Scripts/CharacterBody3D.gd
+++ Scripts/CharacterBody3D.gd
@@ -6,9 +6,10 @@ const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+@onready var ocean = $"../Water"
 
 func _physics_process(delta):
+	RenderingServer.global_shader_parameter_set("ocean_pos", self.position);
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -22,9 +23,16 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
 	print(input_dir)
+	#ocean.position = lerp(ocean.position,self.position,delta*0.1)
+	if input_dir.y == 0 and input_dir.x == 0:
+		ocean.position.x = self.position.x
+		ocean.position.z = self.position.z
+		
 	if input_dir.y != 0:
 		rotate_y(-input_dir.x/50)
 	elif input_dir.y == 0:
+		ocean.position.x = lerp(ocean.position.x,self.position.x,delta*0.1)
+		ocean.position.z = lerp(ocean.position.z,self.position.z,delta*0.1)
 		print("rotating")
 		rotate_y(-input_dir.x/(50*2))
 	if input_dir.y > 0:
@@ -39,5 +47,6 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
 	move_and_slide()
+	
