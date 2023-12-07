85a027301fbcd560a524df8321bda90fedda7efd
zKlau <37957524+zKlau@users.noreply.github.com>
2023-12-07 09:09:01 +0200

    added ship menu animations

--- Scenes/Player_ShipMenu.gd
+++ Scenes/Player_ShipMenu.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var _name = $Name
 @onready var ship = $".."
 @export var cannonball_component : PackedScene
+@onready var anim = $UI_Animations
+var ship_menu_anim : bool = false
 # Called when the node enters the scene tree for the first time.
 func render_projectiles():
 	for i in ship._cannons:
@@ -11,13 +13,22 @@ func render_projectiles():
 		obj.set_level(i.level)
 		obj.set_sprite(i.texture)
 		obj.set_id(ship._cannons.find(i))
-		$HBoxContainer.add_child(obj)
+		$CannonBalls.add_child(obj)
 	pass
 
 func _ready():
 	render_projectiles()
 	pass # Replace with function body.
 
+func _input(event):
+	if event.is_action_pressed("ship_menu") and !anim.is_playing():
+		if ship_menu_anim:
+			anim.play("Ship_menu_out")
+			ship_menu_anim = false
+		else:
+			anim.play("Ship_menu_in")
+			ship_menu_anim = true
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
