81c3781385d3aee9be8c9c3c8846914e755d2eb6
zKlau <37957524+zKlau@users.noreply.github.com>
2024-01-21 20:45:33 +0200

    new shop items object

---
+++ Prefabs/User Interface/SHOP/shop_item.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export var _name : String
+@export_multiline var _description : String
+@export var _price : int
+@export var _stock : int
+@export_enum("Coins","Pearls") var _method : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
